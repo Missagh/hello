e047c6561a7a90f3fb5c649325e4a6f4fccaff0d
Missagh <missagh.sadeghi@gmail.com>
2016-03-02 11:06:45 -0500

    Go getting real

--- hello.go
+++ hello.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Missagh/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
+
+/*
+package main
+
 import "fmt"
 
 func main() {
     fmt.Printf("hello, world\n")
 }
+*/
