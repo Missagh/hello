d7cf6650035f39a4aef370178399ba2cc8d84877
Missagh <missagh.sadeghi@gmail.com>
2016-04-12 17:02:19 -0400

    Learning Switch-case

--- hello.go
+++ hello.go
@@ -6,5 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("!oG ,olleH") + "\n")
+	
+	t := 10;
+	fmt.Printf("The value of the T is %d\n", t)
+	
+	switch {
+	case t == 10:
+		fmt.Printf("T is %d\n", t)
+	}
 }
